b9d4a0fb0fc556d423d4baa3a2923fb6aa2816e9
G-SCHNEYDER <guillaume.schneyder@gmail.com>
2023-10-14 17:02:16 +0200

    Correction bug composer sur dépendances + ajout user interface

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,15 +8,4 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    public function getCacheDir(): string
-    {
-        // for docker performance
-        if ($this->getEnvironment() === 'test' || $this->getEnvironment() === 'dev') {
-            return '/tmp/'.$this->environment;
-        } else {
-            return $this->getProjectDir().'/var/cache/'.$this->environment;
-        }
-
-    }
 }
